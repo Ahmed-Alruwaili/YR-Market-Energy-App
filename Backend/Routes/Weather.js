39e9387a78c30405ec22c51d05d04fc691e1857a
Prayen Shrestha <pshrestha@deakin.edu.au>
2018-09-12 20:32:56 +1000

    Adding location to the weatherAPI and integrating GoogleMap

--- Backend/Routes/Weather.js
+++ Backend/Routes/Weather.js
@@ -10,23 +10,48 @@ weather.use(cors());
 
 process.env.SECRET_KEY = "projectdesign";
 
-weather.get('/getData', function(req, res) {
+weather.post('/getData', function(req, res) {
     var date = new Date();
+    var locationId = req.headers.first_name;
+    //console.log(req.headers.last_name + ":");
+    var locationName = req.headers.last_name + ",AU";
 
     database.connection.getConnection(function(err, connection) {
         if (err) {
         } else {
-            connection.query('SELECT * FROM weather order by 1 desc limit 1', function(err, rows, fields) {
+
+            console.log(locationName);
+            connection.query('SELECT * FROM weather where suburbId = "' + locationId + '" order by 1 desc limit 1', function(err, rows, fields) {
                 if (!err) {
 
                     try{
+                        if(rows.length == 0){
+                          request('http://api.openweathermap.org/data/2.5/forecast?q=' + locationName + '&cnt=100&units=metric&APPID=908f958d6609dd37ee33114a87b81e1a', function (error, response, body) {
+                            if (!error && response.statusCode == 200) {
+                                var saveData = saveWeatherData(body, locationId);
+
+                                if(saveData) {
+                                  connection.query('SELECT * FROM weather order by 1 desc limit 1', function(errChild, rowChild, fieldChild) {
+                                    if (!errChild) {
+                                        res.status(200).json(rowChild);
+                                    }else {
+                                        res.status(204).json("Error Result");
+                                    }
+                                  });
+                                } else {
+                                    res.status(204).json("Error Result");
+                                }
+                            }
+                          });
+                        }
+
                         var dbDate = rows[0].created.getMonth() + "/" + rows[0].created.getDay() + "/" + rows[0].created.getYear();
                         var todayDate = date.getMonth() + "/" + date.getDay() + "/" + date.getYear();
                         if(dbDate != todayDate){
                             console.log("there");
-                            request('http://api.openweathermap.org/data/2.5/forecast?q=Melbourne,AU&cnt=100&units=metric&APPID=908f958d6609dd37ee33114a87b81e1a', function (error, response, body) {
+                            request('http://api.openweathermap.org/data/2.5/forecast?q=' + locationName + '&cnt=100&units=metric&APPID=908f958d6609dd37ee33114a87b81e1a', function (error, response, body) {
                                 if (!error && response.statusCode == 200) {
-                                    var saveData = saveWeatherData(body);
+                                    var saveData = saveWeatherData(body, locationId);
 
                                     if(saveData) {
                                         connection.query('SELECT * FROM weather order by 1 desc limit 1', function(errChild, rowChild, fieldChild) {
@@ -59,26 +84,94 @@ weather.get('/getData', function(req, res) {
     });
 });
 
-function saveWeatherData(weatherresult){
-      var today = new Date();
-      var returnVal = true;
+weather.get('/getLocation', function(req, res) {
+
+    var appData = {};
+
+    database.connection.getConnection(function(err, connection) {
+        if (err) {
+            appData["error"] = 1;
+            appData["data"] = "Internal Server Error";
+            res.status(500).json(appData);
+        } else {
+            connection.query('SELECT * FROM suburb', function(err, rows, fields) {
+                if (!err) {
+                    appData["error"] = 0;
+                    appData["data"] = rows;
+                    res.status(200).json(appData);
+                } else {
+                    appData["data"] = "No data found";
+                    res.status(204).json(appData);
+                }
+            });
+            connection.release();
+        }
+    });
+});
+
+function saveWeatherData(weatherresult, suburbId){
+  var today = new Date();
+  var returnVal = true;
+  var appData = {
+      "error": 1,
+      "data": ""
+  };
+
+  var weatherData = {
+      "Data": weatherresult,
+      "SuburbId": suburbId,
+      "created": today
+  }
+
+  console.log(weatherData);
+
+  database.connection.getConnection(function(err, connection) {
+      if (err) {
+        returnVal = false;
+      } else {
+          connection.query('INSERT INTO weather SET ?', weatherData, function(err, rows, fields) {
+              if (err) {
+                try{
+                  returnVal = false;
+                }catch(ex){
+
+                }
+              }
+          });
+          connection.release();
+      }
+  });
+
+  process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ', err);
+  });
+
+  setTimeout(function () {
+    console.log('This will still run.');
+  }, 500);
+  return returnVal;
+}
+
+function getSuburbLocation(locationId){
+      var returnVal = "melbourne,VIC,AUS";
       var appData = {
           "error": 1,
           "data": ""
       };
 
-      var weatherData = {
-          "Data": weatherresult,
-          "created": today
-      }
-
       database.connection.getConnection(function(err, connection) {
           if (err) {
             returnVal = false;
           } else {
-              connection.query('INSERT INTO weather SET ?', weatherData, function(err, rows, fields) {
-                  if (err) {
-                      returnVal = false;
+              connection.query('SELECT ActualValue FROM suburb WHERE Id = ?', [locationId], function(err, rows, fields) {
+                  if (!err) {
+                      try{
+                        //console.log(rows[0].ActualValue);
+                        returnVal = rows[0].ActualValue;
+                      }catch(ex){
+
+                      }
+                      
                   }
               });
               connection.release();
--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,52 +1,155 @@
 
   $(document).ready(function(){
+    window.locationData = [];
 
-      $('.js-scroll-trigger').click(function () {
-        var id = $(this).attr("href");
-        //console.log();
-        var totalheight = ($(window).height() - $(id).height()) *5;
-        //console.log(totalheight);
-        $('html, body').animate({scrollTop : $(id).offset().top},800);
-        return false;
+    $.get("http://localhost:3000/weather/getLocation", function(response){
+      response = response.data;
+      $.each(response, function(i, v){
+        var valFormat = {};
+        valFormat["key"] = v.Id;
+        valFormat["value"] = v.Name;
+        locationData.push(valFormat);
       });
-
-      $(".navbar-toggler").on("click", function(){
-        $("#navbarResponsive").toggle();
+      locationData.sort(function (a, b) {
+          return a.value.localeCompare(b.value);
       });
+      
+      $('#search-location').append($('<option>', {value:"", text:'Select'}));      
+      //console.log(locationData);
+      $.each(locationData, function(i, v){
+        ///console.log("hello");
+        $('#search-location').append($('<option>', { 
+            value: v.key,
+            text : v.value 
+        }));
+      });
+    });
 
-      searchBar();
+    
 
-      $("#calendar-btn").on("click", function(e){
-        e.preventDefault();
-        
+    $('.js-scroll-trigger').click(function () {
+      var id = $(this).attr("href");
+      //console.log();
+      var totalheight = ($(window).height() - $(id).height()) *5;
+      //console.log(totalheight);
+      $('html, body').animate({scrollTop : $(id).offset().top},800);
+      return false;
+    });
+
+    $(".navbar-toggler").on("click", function(){
+      $("#navbarResponsive").toggle();
+    });
+
+    searchBar();
+
+    $("#calendar-btn").on("click", function(e){
+      e.preventDefault();
+      
+      $(".search-date").focus();
+    });
+
+    $("#search-location").on("change", function(e){
+      e.preventDefault();
+      if($("#search-location option:selected").val() == ""){
+        alert("Please select a valid location");
+        $("#search-location").focus();
+        return;
+      }else if($('.search-date').val() == ""){ 
+        alert("Please select a day");
         $(".search-date").focus();
-      });
+        return;
+      }
 
-      $('.search-date').datepicker({ dateFormat: 'yy-mm-dd' }).on('changeDate', function (ev) {
-        var date = $(this).val().split("/");
-        var month = date[0];
-        var day = date[1];
-        var year = date[2];
-        var dateFormat = year + "-" + month + "-" + day;
-        var selectedVal = [];
-        $.get("http://localhost:3000/weather/getData", function(response){
+      var date = $(".search-date").val().split("/");
+      var month = date[0];
+      var day = date[1];
+      var year = date[2];
+      var dateFormat = year + "-" + month + "-" + day;
+      var selectedVal = [];
+
+      $.ajax({
+        url: 'http://localhost:3000/weather/getData',
+        headers: {
+            'first_name':$("#search-location option:selected").val(),
+            'last_name':$("#search-location option:selected").text(),
+            'Content-Type':'application/json'
+        },
+        method: 'POST',
+        dataType: 'json',
+        success: function(response){
           response = JSON.parse(response[0].Data);
           //console.log(response);
+          var latlong = response.city.coord.lat + ", " + response.city.coord.lon;
+
           $.each(response.list, function(i, v){
             var resDate = v.dt_txt.split(" ");
             if(resDate[0] == dateFormat){
               var valFormat = {};
-              valFormat["MaximumTemperature"] = v.main.temp_max + "°C";
-              valFormat["MinimumTemperature"] = v.main.temp_min + "°C";
+              // valFormat["MaximumTemperature"] = v.main.temp_max + "°C";
+              // valFormat["MinimumTemperature"] = v.main.temp_min + "°C";
               valFormat["Temperature"] = v.main.temp + "°C";
               valFormat["Time"] = resDate[1];
+              valFormat["Suburb"] = response.city.name;
+              valFormat["Population"] = response.city.population;
               selectedVal.push(valFormat);
             }
           });
           //console.log(selectedVal)
+          myMap(latlong);
+
+          google.maps.event.trigger(map, 'resize');
           $(".json-data").html(JSON.stringify(selectedVal));
-        });
+
+        }
       });
+    });
+
+    $('.search-date').datepicker({ dateFormat: 'yy-mm-dd' }).on('changeDate', function (ev) {
+      if($("#search-location option:selected").val() == ""){
+        alert("Please select the location");
+        $("#search-location").focus();
+        return;
+      }
+      var date = $(this).val().split("/");
+      var month = date[0];
+      var day = date[1];
+      var year = date[2];
+      var dateFormat = year + "-" + month + "-" + day;
+      var selectedVal = [];
+      $.ajax({
+        url: 'http://localhost:3000/weather/getData',
+        headers: {
+            'first_name':$("#search-location option:selected").val(),
+            'last_name':$("#search-location option:selected").text(),
+            'Content-Type':'application/json'
+        },
+        method: 'POST',
+        dataType: 'json',
+        success: function(response){
+          response = JSON.parse(response[0].Data);
+          console.log(response);
+          var latlong = response.city.coord.lat + ", " + response.city.coord.lon;
+          $.each(response.list, function(i, v){
+            var resDate = v.dt_txt.split(" ");
+            if(resDate[0] == dateFormat){
+              var valFormat = {};
+              // valFormat["MaximumTemperature"] = v.main.temp_max + "°C";
+              // valFormat["MinimumTemperature"] = v.main.temp_min + "°C";
+              valFormat["Temperature"] = v.main.temp + "°C";
+              valFormat["Time"] = resDate[1];
+              valFormat["Suburb"] = response.city.name;
+              valFormat["Population"] = response.city.population;
+              selectedVal.push(valFormat);
+            }
+          });
+          //console.log(latlong)
+          myMap(latlong);
+
+          google.maps.event.trigger(map, 'resize');
+          $(".json-data").html(JSON.stringify(selectedVal));
+        }
+      });
+    });
       
   });
 
@@ -58,3 +161,12 @@
       endDate: '+4d'
     }).datepicker({ dateFormat: 'yy-mm-dd' });
   }
+
+
+function myMap(latLong) {
+  var mapProp= {
+      center:new google.maps.LatLng(-37.8218, 145.1523),
+      zoom:15,
+  };
+  window.map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
